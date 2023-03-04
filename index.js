a75c5d1b0ecd522b77b7ac3f8eeead7cdba53fed
ranizine <yaroslav76071@mail.ru>
2023-03-04 22:32:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath == './') {
+    filePath = './pages/index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.html':
+      contentType = 'text/html';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`<h1>404 Page Not Found</h1><p>The requested URL ${req.url} was not found on this server.</p>`);
+      } else {
+        res.writeHead(500);
+        res.end(`Internal Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+  if (filePath == './old-page') {
+    res.writeHead(301, { 'Location': '/new-page' });
+    res.end();
+    return;
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
